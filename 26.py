61bba01f2958e5f54fbf6e7082fa669acfb2e93a
MKhoroshko <margo.khoroshko@gmail.com>
2025-01-29 17:02:15 +0200

    commit forst

--- 26.py
+++ 26.py
@@ -1,125 +1,125 @@
 # exp1
-class Editor:
-    def view_document(self):
-        print("Перегляд документа.")
-
-    def edit_document(self):
-        print("Редагування документів недоступне для безкоштовної версії.")
-
-class ProEditor(Editor):
-    def edit_document(self):
-        print("Редагування документа доступне.")
-
-key = input("Введіть ліцензійний ключ: ")
-if key == "правильний_ключ":
-    editor = ProEditor()
-else:
-    editor = Editor()
-
-editor.view_document()
-editor.edit_document()
-
-# exp2
-class GraphicObject:
-    def draw(self):
-        print("Малювання графічного об'єкта.")
-
-class Rectangle(GraphicObject):
-    def draw(self):
-        print("Малювання прямокутника.")
-
-class ClickableObject:
-    def click(self):
-        print("Об'єкт натиснуто.")
-
-class Button(ClickableObject):
-    def click(self):
-        print("Кнопка натиснута.")
-
-
-button = Button()
-rectangle = Rectangle()
-
-button.click()
-rectangle.draw()
-
-#exp3
-
-class A:
-    def method(self):
-        print("Метод класу A.")
-
-class B(A):
-    def method(self):
-        print("Метод класу B.")
-        super().method()
-
-class C(A):
-    def method(self):
-        print("Метод класу C.")
-        super().method()
-
-class D(B, C):
-    def method(self):
-        print("Метод класу D.")
-        super().method()
-
-d = D()
-d.method()
-
-# exp5
-def adult_check(func):
-    def adult_check(func):
-        def wrapper(*args, *kwargs):
-            age = func(*args, *kwargs)
-            return age >= 18
-
-        return wrapper
-
-    @adult_check
-    def age_in_ukraine(years):
-        return years
-
-@adult_check
-def age_in_usa(years):
-    return years
-
-# exp6
-def count_adults(func):
-    def wrapper(cls):
-        objects = func(cls)
-        adults = [obj for obj in objects if obj.age >= 18]
-        return len(adults)
-
-    return wrapper
-
-class Person:
-    def __init__(self, age):
-        self.age = age
-
-@count_adults
-def get_people(cls):
-    return [cls(20), cls(16), cls(30)]
-
-# exp7
-class Vehicle:
-    def __init__(self, brand, model):
-        self.brand = brand
-        self.model = model
-
-    def info(self):
-        print(f"Марка: {self.brand}, Модель: {self.model}")
-
-class Car(Vehicle):
-    def info(self):
-        print(f"Автомобіль - Марка: {self.brand}, Модель: {self.model}")
-
-class Bicycle(Vehicle):
-    def info(self):
-        print(f"Велосипед - Марка: {self.brand}, Модель: {self.model}")
-
-car = Car("Toyota", "Camry")
-bicycle = Bicycle("Giant", "Escape 3")
-
-car.info()
-bicycle.info()
+# class Editor:
+#     def view_document(self):
+#         print("Перегляд документа.")
+#
+#     def edit_document(self):
+#         print("Редагування документів недоступне для безкоштовної версії.")
+#
+# class ProEditor(Editor):
+#     def edit_document(self):
+#         print("Редагування документа доступне.")
+#
+# key = input("Введіть ліцензійний ключ: ")
+# if key == "правильний_ключ":
+#     editor = ProEditor()
+# else:
+#     editor = Editor()
+#
+# editor.view_document()
+# editor.edit_document()
+#
+# # exp2
+# class GraphicObject:
+#     def draw(self):
+#         print("Малювання графічного об'єкта.")
+#
+# class Rectangle(GraphicObject):
+#     def draw(self):
+#         print("Малювання прямокутника.")
+#
+# class ClickableObject:
+#     def click(self):
+#         print("Об'єкт натиснуто.")
+#
+# class Button(ClickableObject):
+#     def click(self):
+#         print("Кнопка натиснута.")
+#
+#
+# button = Button()
+# rectangle = Rectangle()
+#
+# button.click()
+# rectangle.draw()
+#
+# #exp3
+#
+# class A:
+#     def method(self):
+#         print("Метод класу A.")
+#
+# class B(A):
+#     def method(self):
+#         print("Метод класу B.")
+#         super().method()
+#
+# class C(A):
+#     def method(self):
+#         print("Метод класу C.")
+#         super().method()
+#
+# class D(B, C):
+#     def method(self):
+#         print("Метод класу D.")
+#         super().method()
+#
+# d = D()
+# d.method()
+#
+# # exp5
+# def adult_check(func):
+#     def adult_check(func):
+#         def wrapper(*args, *kwargs):
+#             age = func(*args, *kwargs)
+#             return age >= 18
+#
+#         return wrapper
+#
+#     @adult_check
+#     def age_in_ukraine(years):
+#         return years
+#
+# @adult_check
+# def age_in_usa(years):
+#     return years
+#
+# # exp6
+# def count_adults(func):
+#     def wrapper(cls):
+#         objects = func(cls)
+#         adults = [obj for obj in objects if obj.age >= 18]
+#         return len(adults)
+#
+#     return wrapper
+#
+# class Person:
+#     def __init__(self, age):
+#         self.age = age
+#
+# @count_adults
+# def get_people(cls):
+#     return [cls(20), cls(16), cls(30)]
+#
+# # exp7
+# class Vehicle:
+#     def __init__(self, brand, model):
+#         self.brand = brand
+#         self.model = model
+#
+#     def info(self):
+#         print(f"Марка: {self.brand}, Модель: {self.model}")
+#
+# class Car(Vehicle):
+#     def info(self):
+#         print(f"Автомобіль - Марка: {self.brand}, Модель: {self.model}")
+#
+# class Bicycle(Vehicle):
+#     def info(self):
+#         print(f"Велосипед - Марка: {self.brand}, Модель: {self.model}")
+#
+# car = Car("Toyota", "Camry")
+# bicycle = Bicycle("Giant", "Escape 3")
+#
+# # car.info()
+# bicycle.info()
---
+++ 28.py
@@ -0,0 +1,117 @@
+# exp1
+class Car:
+    def __init__(self, make, model, year):
+        self.__make = make
+        self.__model = model
+        self.__year = year
+
+    def get_make(self):
+        return self.__make
+
+    def set_make(self, make):
+        self.__make = make
+
+    def get_model(self):
+        return self.__model
+
+    def set_model(self, model):
+        self.__model = model
+
+    def get_year(self):
+        return self.__year
+
+    def set_year(self, year):
+        self.__year = year
+
+# exp 2
+class English:
+    def greeting(self):
+        return "Hello, friend!"
+
+class Spanish:
+    def greeting(self):
+        return "¡Hola, amigo!"
+
+def hello_friend():
+    eng = English()
+    esp = Spanish()
+    print(eng.greeting())
+    print(esp.greeting())
+
+hello_friend()
+
+# exp3
+class Temperature:
+    def __init__(self, celsius):
+        self._celsius = celsius
+
+    @property
+    def celsius(self):
+        return self._celsius
+
+    @property
+    def fahrenheit(self):
+        return self._celsius * 9/5 + 32
+
+    @celsius.setter
+    def celsius(self, value):
+        self._celsius = value
+
+    @fahrenheit.setter
+    def fahrenheit(self, value):
+        self._celsius = (value - 32) * 5/9
+
+# exp4
+class Base:
+    @classmethod
+    def method(cls):
+        print("Hello from Base")
+
+class Child(Base):
+    @classmethod
+    def method(cls):
+        print("Hello from Child")
+
+Base.method()
+Child.method()
+
+# exp5
+import matplotlib.pyplot as plt
+import numpy as np
+
+class Shape:
+    def area(self):
+        pass
+
+class Cone(Shape):
+    def __init__(self, radius, height):
+        self.radius = radius
+        self.height = height
+
+    def area(self):
+        return np.pi * self.radius * (self.radius + np.sqrt(self.height*2 + self.radius*2))
+
+class Paraboloid(Shape):
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+    def area(self):
+        return np.double(np.pi * self.a * self.b)
+
+cone = Cone(3, 4)
+paraboloid = Paraboloid(2, 3)
+
+print("Cone area:", cone.area())
+print("Paraboloid area:", paraboloid.area())
+
+# Visualization example (paraboloid)
+X = np.linspace(-3, 3, 100)
+Y = np.linspace(-3, 3, 100)
+X, Y = np.meshgrid(X, Y)
+Z = (X*2) / 4 + (Y*2) / 9  # Example equation for a paraboloid
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.plot_surface(X, Y, Z, alpha=0.5)
+plt.show()
